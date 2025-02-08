05a800afcf3e4efc5cd068bca6322989c3b990ad
ArpanPrj <azp0208@auburn.edu>
2025-02-07 19:19:31 -0600

    editing again final time

--- Assignment inclass.R
+++ Assignment inclass.R
@@ -14,13 +14,13 @@ colnames(zdf) <- c("zvec", "zlogic")# Change column names to "zvec" and "zlogic"
 zdf$zsquared <- zdf$zvec^2# Add a new column 'zsquared' which is zvec squared
 
 
-subset_zdf1 <- subset(zdf, zsquared > 10 & zsquared < 100) # Subset the dataframe to include only values where zsquared is >10 and <100 (using subset function)
+subset_zdf1 <- subset(zdf$zsquared, zdf$zsquared > 10 & zdf$zsquared < 100) # Subset the dataframe to include only values where zsquared is >10 and <100 (using subset function)
 
 
-subset_zdf2 <- zdf[zdf$zsquared > 10 & zdf$zsquared < 100, ] # Subset the dataframe using logical indexing
+subset_zdf2 <- zdf$zsquared[zdf$zsquared > 10 & zdf$zsquared < 100] # Subset the dataframe using logical indexing
 
 
-row_26 <- zdf$zvec[26]# Subset the zdf dataframe to only include row 26 values
+row_26 <- zdf[26,]# Subset the zdf dataframe to only include row 26 values
 
 zsquared_180 <- zdf$zsquared[180] #Subset the zdf dataframe to get only the zsquared value in the 180th row
 # we can also do this: zsquared_180<-zdf[180,3] as the zsquared is in the third column
@@ -33,6 +33,4 @@ print(zsquared_180)
 
 tips_data <- read.csv("https://raw.githubusercontent.com/ArpanPrj/Reproducibility2025/refs/heads/main/TipsR.csv", na.strings = ".") ## Read the CSV file and treat "." as NA
 
-# To know if the data was correctly uploaded
-summary(tips_data) 
-str(tips_data)
+
